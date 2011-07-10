db86e68fdf20028ad3940dc0810528ee49bc1ff1
Peter Curd <peter@pcurd.co.uk>
2011-07-10 12:32:30 +0100

    Adding libraries for Graphing

---
+++ MyFitnessLibrary/Graphing/IGraphCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace MyFitnessLibrary.Graphing
+{
+    interface IGraphCreator
+    {
+
+        Image GetImage(IGraphOptions graphOptions)
+        {
+        }
+    }
+
+    interface IGraphOptions
+    {
+
+    }
+}
